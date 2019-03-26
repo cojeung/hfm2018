812ff7991ea1306f3faa8d753e43e939f975123b
Courtney Jeung <cojeung@ucdavis.edu>
2019-03-25 23:04:13 -0700

    Update hfm.js

--- hfm.js
+++ hfm.js
@@ -1,8 +1,8 @@
 function addMessage() {
 
-  var msgStr = 'As the original site domain has expired due to the frequency of the ' +
-  'conference, this is a re-uploaded version.  ' +
-  'All official changes made after 1/1/2019 will not be reflected here.';
+  var msgStr1 = 'As the original site domain has expired due to the frequency of the ' +
+  'conference, this is a re-uploaded version.';
+  var msgStr2 = 'All official changes made after 1/1/2019 will not be reflected here.';
 
   var header = document.getElementsByTagName("header")[0]; //enter header box
   var div_to_add = document.createElement("div"); //make a div
@@ -10,8 +10,14 @@ function addMessage() {
   div_to_add.style.color = "red";
   div_to_add.style.padding = "10px";
 
-  var msg_to_add = document.createTextNode(msgStr); //make chunk of text
-  div_to_add.appendChild(msg_to_add); //stick message into div
+  var msg_to_add1 = document.createTextNode(msgStr1); //make chunk of text
+  var msg_to_add2 = document.createTextNode(msgStr2);
+
+  var newline = document.createElement("br"); //create new line
+
+  div_to_add.appendChild(msg_to_add1); //stick message into div
+  div_to_add.appendChild(newline);
+  div_to_add.appendChild(msg_to_add2);
 
   header.appendChild(div_to_add); //add to header
 }
